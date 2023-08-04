f23e626f59b58824ff947cefa309c878518cb1a8
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-08-04 14:48:40 +0200

    add validation and seeds

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
 
   has_many :topics, dependent: :destroy
 
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 } 
+  validates_uniqueness_of :name
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Create categories
+categories = [
+  { name: 'Sports' },
+  { name: 'History' }
+]
+
+categories.each do |category|
+  Category.create!(category)
+end
+
+#Users
+# User.create!(:nickname => 'John Doe', :email => 'john@gmail.com', :password => 'password', :password_confirmation => 'password')
+# User.create!(:nickname => 'John Morgan', :email => 'morgan@gmail.com', :password => 'password1', :password_confirmation => 'password1')
